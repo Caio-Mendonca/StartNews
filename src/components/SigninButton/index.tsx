8aa1b4978c3c112460f4ecb7e6fa704053289b53
Caio-Mendonca <caioeduardojm4@gmail.com>
2022-07-06 22:18:04 -0400

    rewrite

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,9 +1,24 @@
 import { FaGithub } from 'react-icons/fa'
+import { FiX } from 'react-icons/fi'
+
+import styles from './style.module.scss'
 
 export function SigninButton(){
-    return (
-        <button type="button">
-            <FaGithub/>
+    const isUserLoggedIn = true
+
+    return isUserLoggedIn ? (
+        <button 
+        type="button" 
+        className={styles.signinButton}>
+            <FaGithub color='#04d361'/>
+            Caio Mendonca
+            <FiX color ='#737380' className={styles.closeIcon}/>
+        </button>
+    ) : (
+        <button 
+        type="button" 
+        className={styles.signinButton}>
+            <FaGithub color='#ff9900'/>
             Login com o Github
         </button>
     )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import styles from './home.module.scss'
 export default function Home() {
   return (
     <>      
@@ -6,9 +7,30 @@ export default function Home() {
         <title>Start Home</title>
         <meta name='descrption' content='Start Criação e Desenvolvimento'/>
       </Head>
-      <h1>
-        Hello World!!
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span> 
+            <img 
+            src="https://raw.githubusercontent.com/iampavangandhi/iampavangandhi/master/gifs/Hi.gif" 
+            width="20px" 
+            height="20px"
+            alt='icone de mao dando boas vindas'>
+            </img> 
+            Olá, Bem-Vindo(a)
+          </span>
+            <h1>
+              Os melhores conteúdos<br/> 
+              sobre <span>PROGRAMAÇÃO</span>
+            </h1>
+        </section>
+        <img src='/images/SVGapresentacao.png' alt='imagem de foguete com logo da empresa'/>
+      </main>
+      <footer className={styles.footerStyle}>
+        <h1>Powerd By</h1>
+        <img        
+        src="/images/STARTlogo.png" 
+        alt="Logo start criacao e desenvolvimento"/>
+      </footer>
     </>
   )
 }
