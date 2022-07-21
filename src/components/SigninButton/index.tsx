9c5677b320776b2f8579436c0fe8f5434d7b3825
Caio-Mendonca <caioeduardojm4@gmail.com>
2022-07-20 22:51:18 -0400

    rewrite

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,25 +1,27 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
-
+import { signIn, signOut, useSession }  from 'next-auth/react'
 import styles from './style.module.scss'
 
 export function SigninButton(){
-    const isUserLoggedIn = true
-
-    return isUserLoggedIn ? (
+    const { data: session } = useSession()
+    
+    return session ? (
         <button 
         type="button" 
-        className={styles.signinButton}>
+        className={styles.signinButton}
+        onClick={()=> signOut()}>
             <FaGithub color='#04d361'/>
-            <h4 className={styles.userName}>Caio Mendonca</h4>
+            <h4 className={styles.userName}>{session.user.name}</h4>
             <FiX color ='#737380' className={styles.closeIcon}/>
         </button>
     ) : (
         <button 
         type="button" 
+        onClick={() => signIn('github')}
         className={styles.signinButton}>
             <FaGithub color='#ff9900'/>
-            Login com o Github
+            <h4 className={styles.userName}> Login com o Github</h4>
         </button>
     )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,24 @@
 import { AppProps } from "next/app"
 import React from "react";
 import { Header } from "../components/Header";
+import { SessionProvider as NextAuthProvider }  from 'next-auth/react'
+
 import '../styles/global.scss';
 function MyApp({ Component, pageProps } : AppProps) {
   React.useEffect(() => {
     if (typeof window !== 'undefined') {
-            const loader = document.getElementById('globalLoader');
-        if (loader)
+      const loader = document.getElementById('globalLoader');
+    if (loader)
             loader.style.display = 'none';
     }
   }, []);
   return (
     <>
-      <Header/>
-      <Component {...pageProps}/>
-    </>
+      <NextAuthProvider session={pageProps.session}>
+        <Header/>
+        <Component {...pageProps}/>
+      </NextAuthProvider>
+    </> 
   )
 }
 
