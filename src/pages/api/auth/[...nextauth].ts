5e520557d939a5097deac6204204d8a2d801a0dd
Caio-Mendonca <caioeduardojm4@gmail.com>
2022-07-21 00:07:07 -0400

    rewrite

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,8 +5,8 @@ export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
-      clientId: 'efa404e2114ef2c7d8b5',
-      clientSecret: 'a494ae2a83e954e62fcaa96cbde8379b41408521',
+      clientId: process.env.GITHUB_CLIENT_ID_PROD,
+      clientSecret: process.env.GITHUB_SECRET_PROD,
       authorization: {
         params: {
           scope: 'read:user',
