7e0022057920233be262e3eacdaa55bbe40bad6b
Caio-Mendonca <caioeduardojm4@gmail.com>
2022-07-24 13:34:00 -0400

    rewrite

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
+import { query as q } from "faunadb"
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+import { fauna } from '../../../services/fauna'
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -14,4 +16,22 @@ export default NextAuth({
       },    
     }),
   ],
+  callbacks:{
+    async signIn({user, account, profile}){
+      console.log(user)
+      const {email} = user
+      try{
+        await fauna.query(
+        q.Create(
+          q.Collection('users'),
+          {data: {email}}
+        )
+      )
+      
+      return true
+      }catch{
+        return false
+      }
+    }
+  }
 })
